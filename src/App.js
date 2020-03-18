6957c6240c4126f79861fdfa94d111ba6b24fe2c
VanTigranyan <tigranyan.van@gmail.com>
2020-03-18 21:57:58 +0400

    uuid generation function

--- src/App.js
+++ src/App.js
@@ -12,6 +12,16 @@ const identityPoolId = process.env.REACT_APP_AWS_IDENTITY_POOL_ID;
 
 console.log(bucketName, awsRegion, identityPoolId);
 
+function generateUUID() {
+  let d = new Date().getTime();
+  const uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    const r = (d + Math.random()*16)%16 | 0;
+    d = Math.floor(d/16);
+    return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+  });
+  return uuid;
+};
+
 const credentials = new AWS.CognitoIdentityCredentials({
   IdentityPoolId: identityPoolId
 });
